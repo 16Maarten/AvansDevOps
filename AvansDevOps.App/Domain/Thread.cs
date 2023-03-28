159229261134d16516b5da78415e1713880fc8c3
Mo-Gal <mo_@live.nl>
2023-03-28 18:44:43 +0200

    Added Forum, refactored Persons

---
+++ AvansDevOps.App/Domain/Thread.cs
@@ -0,0 +1,22 @@
+﻿using AvansDevOps.App.Domain.Users;
+
+namespace AvansDevOps.App.Domain;
+
+public class Thread : Responsive
+{
+    private string _title;
+
+    public Thread(string title, string message, Person person) : base(message, person) {
+        _title = title;        
+    }
+
+    public override string ToStringWithoutNested()
+    {
+        return $"------------\nThread: \"{_title}\"\n------------\n{base.ToStringWithoutNested()}";
+    }
+
+    public override string ToString()
+    {
+        return $"------------\nThread: \"{_title}\"\n------------\n{base.ToString()}";
+    }
+}
